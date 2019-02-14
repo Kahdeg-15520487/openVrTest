4bba437a5c4910c905fac6e086ada41d00555fe8
Minh <15520487@gm.uit.edu.vn>
2019-02-14 10:02:29 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Valve.VR;
+
+namespace openVrTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var error = EVRInitError.None;
+            var vrSystem = OpenVR.Init(ref error, EVRApplicationType.VRApplication_Other);
+            if (error != EVRInitError.None)
+            {
+                // handle init error
+                Console.WriteLine(error);
+            }
+        }
+    }
+}
